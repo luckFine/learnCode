da4f2c358a0f6c03f7a9701a7c1db1a3e4728449
mingzi.yuan <mingzi.yuan@jrj.com.cn>
2020-09-24 18:07:52 +0800

    3.0

---
+++ vue3/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  setup(props,context){
+    console.log(props)
+    console.log(context)
+    console.log(this)
+  }
+}
+</script>
